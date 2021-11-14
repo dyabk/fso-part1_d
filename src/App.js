aa9d6d2fb74bcd93563681d2836ac53ce05d66f2
dyabk <dzmitry.yabkevich@outlook.com>
2021-11-14 18:46:03 -0500

    Implement conditional rendering of the clicking history by adding a new component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
 import React, { useState } from 'react'
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
@@ -21,6 +37,7 @@ const App = () => {
       <button onClick={handleRightClick}>right</button>
       {right}
       <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks} />
     </div>
   )
 }
