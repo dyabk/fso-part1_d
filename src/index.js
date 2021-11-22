bc6f4cbfccb83975011ebe56c04c25053fcadbae
dyabk <dzmitry.yabkevich@outlook.com>
2021-11-22 16:43:14 -0500

    Changed the App component.

--- src/index.js
+++ src/index.js
@@ -23,25 +23,12 @@ const Button = ({ handleClick, text }) => (
   )
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
+  const [value, setValue] = useState(10)
 
   return (
     <div>
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={handleRightClick} text='right' />
-      <History allClicks={allClicks} />
+      {value}
+      <button>reset to zero</button>
     </div>
   )
 }
